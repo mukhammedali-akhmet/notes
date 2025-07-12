0f8628bee6b3d2d476f92a25762e98befc2730dc
You can call me Ali <zizo.rob.mine@gmail.com>
2025-07-12 22:44:19 +0500

    preparetion for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,41 @@
 import express from "express";
-import dotenv from "dotenv"
-import cors from "cors"
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './routes/notesRoutes.js'
-import { getAllNotes } from "./controllers/notesController.js";
+import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+app.use(express.json());
+app.use(rateLimiter);
 
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
-app.use(express.json())
-app.use(rateLimiter)
+app.use("/api/notes", notesRoutes);
 
-app.use("/api/notes", notesRoutes)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running in ${PORT}`)
-    })
-})
+        console.log("Server started on PORT:", PORT);
+    });
+});
